e8401e9b7e997b2193c3a042507db09253a0277f
dharnesh13600 <dharnesh.indumathirameshku@ucdconnect.ie>
2025-06-17 23:06:31 +0100

    ADDED files

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Map from './components/map';
+// import Dummy from './components/dummy';
+
+export default function Page(){
+    return (
+        <>
+            <Map/>
+        </>
+    );
+}
