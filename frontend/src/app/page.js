904b9e0580ccac42f14f3944563d018ab2d68f58
DiviyyaShreeIyappan <diviyya.iyappan@ucdconnect.ie>
2025-06-21 23:01:26 +0100

    Update page.js
    fixing lint bugs

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,14 +5,14 @@ export default function Page(){
         <>
                <div className='hero-body flex'>
                 <div>
-                    <Image className='hero-image' src='/statue_of_liberty.jpg' alt='description' width={400} height={400}/>
+                    <Image className='hero-image' src='/statue_of_liberty.jpg' alt="description" width={400} height={400}/>
                 </div>
                 <div className='hero-content'>
                     <h2>Welcome to Manhattan Muse! <br></br>
 Your creative guide to New York City.
 </h2>
 <br></br>
-<p>Designed for artists, performers, photographers, and creators. Simply choose your activity and time our interactive map helps you discover the best places in Manhattan to express your work. Whether you're looking for a high-traffic corner to busk, a quiet park to sketch, or the perfect light for portrait photography, Manhattan Canvas lets you plan with precision—using real-time data on crowds, weather, and more.</p>
+<p>Designed for artists, performers, photographers, and creators. Simply choose your activity and time&mdash; our interactive map helps you discover the best places in Manhattan to express your work. Whether you&rsquo;re looking for a high-traffic corner to busk, a quiet park to sketch, or the perfect light for portrait photography, Manhattan Canvas lets you plan with precision&mdash;using real-time data on crowds, weather, and more.</p>
 <Button />
                 </div>
             </div>
