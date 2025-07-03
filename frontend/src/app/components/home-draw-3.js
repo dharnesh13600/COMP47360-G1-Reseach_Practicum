3e03487a2e98b55d061490df2fdf4e97973d2f53
Phirada Kanjanangkulpunt <phiradakan23@hotmail.com>
2025-07-03 20:53:30 +0100

    arrows in home page
    add arrows in front end home page

---
+++ frontend/src/app/components/home-draw-3.js
@@ -0,0 +1,22 @@
+
+export default function Draw03(){
+    return(
+        <>
+            <div className='draw03'>
+                <svg height="200" width="400">
+                    <path d='M95,0 
+                    Q80,20 110,25
+                    Q150,30 100,45
+                    Q40,65 100,70
+                    Q160,70 165,75
+                    Q170,80 150,85
+                    Q80,100 85,120
+                    Q90,130 110,125
+                    Q160,110 110,180
+                    ' 
+                    fill='none' stroke='orange' strokeWidth='1'/>
+                </svg>
+            </div>
+        </>
+    );
+}
