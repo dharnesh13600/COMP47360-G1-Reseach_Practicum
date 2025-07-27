da74e40e4ab961d6fd2548f348a458504a307120
dharnesh13600 <dharnesh.indumathirameshku@ucdconnect.ie>
2025-07-27 21:47:37 +0100

    updated cicd for frontend

--- frontend/kube-deploy.sh
+++ frontend/kube-deploy.sh
@@ -9,23 +9,19 @@ set -e
 : "${LOCATION:?LOCATION (zone) must be set}"
 : "${IMAGE_URI:?IMAGE_URI (full repo URI) must be set}"
 
-# GCP_KEYFILE_PATH="$1"
+
 GCP_KEYFILE_PATH="${GCP_KEYFILE}" 
 
 echo "DEBUG (in kube-deploy.sh): GCP_KEYFILE_PATH is: ${GCP_KEYFILE_PATH}"
-test -f "${GCP_KEYFILE_PATH}" && echo "DEBUG (in kube-deploy.sh): Key file exists and is a regular file." || echo "DEBUG (in kube-deploy.sh): Key file does NOT exist or is not a regular file."
-test -r "${GCP_KEYFILE_PATH}" && echo "DEBUG (in kube-deploy.sh): Key file is readable." || echo "DEBUG (in kube-deploy.sh): Key file is NOT readable."
 
-#configure kubernetes access
+#configuring kubernetes access
+
 echo "*** setting up kubernetes access based on service account token ***";
 
-# gcloud auth activate-service-account --key-file=${GCP_KEYFILE} --project=${GCP_PROJECT}
 gcloud auth activate-service-account --key-file="${GCP_KEYFILE_PATH}" --project="${GCP_PROJECT}"
-# gcloud container clusters get-credentials ${CLUSTER_NAME} --zone=${LOCATION}
 gcloud container clusters get-credentials "${CLUSTER_NAME}" --zone="${LOCATION}"
 
-
-#kubernetes won't allow variables in the yaml files so using envsubst workaround so we can use them
+# converting from .tmpl to .yaml
 
 echo "*** creating deployment yaml files ***";
 envsubst < k8s/frontend-deployment.tmpl > k8s/frontend-deployment.yaml;
@@ -38,8 +34,9 @@ echo "*** deploying docker container and setting up the service and ingress  ***
 
 # kubectl delete -f k8s/frontend-deployment.yaml --validate=false --insecure-skip-tls-verify=true;
 
+# applying the yaml files
+
 kubectl apply -f k8s/frontend-deployment.yaml --validate=false --insecure-skip-tls-verify=true;
 kubectl apply -f k8s/frontend-service.yaml --validate=false --insecure-skip-tls-verify=true;
 kubectl apply -f k8s/ingress.yaml --validate=false --insecure-skip-tls-verify=true;
 
-# kubectl apply -f k8s/secret.yaml --validate=false --insecure-skip-tls-verify=true;
