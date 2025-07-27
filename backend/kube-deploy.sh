4cb61fdab667b5c7e3c54a76d94dfa42a97b254e
dharnesh13600 <dharnesh.indumathirameshku@ucdconnect.ie>
2025-07-27 21:38:53 +0100

    updated cicd files

--- backend/kube-deploy.sh
+++ backend/kube-deploy.sh
@@ -10,20 +10,25 @@ test -r "${GCP_KEYFILE_PATH}" || { echo "Key not readable"; exit 1; }
 
 echo "DEBUG: IMAGE_URL=${IMAGE_URL}"
 
+# commands for setting up kubernetes access
+
 gcloud auth activate-service-account --key-file="${GCP_KEYFILE_PATH}" --project="${PROJECT_ID}"
 gcloud container clusters get-credentials "${CLUSTER_NAME}" --zone "${LOCATION}"
 
 echo "*** Creating deployment YAML files using envsubst ***"
 mkdir -p output
 
+# Converting from .tmpl to .yaml
+
 env IMAGE_URL="${IMAGE_URL}" envsubst < k8s/backend-deployment.tmpl > output/backend-deployment.yaml
 env envsubst < k8s/backend-service.tmpl > output/backend-service.yaml
-# render ingress too (if this pipeline owns it)
 env envsubst < k8s/ingress.tmpl > output/ingress.yaml
+
+# Commands for applying yaml files
 
 echo "*** Deploying ***"
 kubectl apply -f output/backend-deployment.yaml
 kubectl apply -f output/backend-service.yaml
-kubectl apply -f output/ingress.yaml   # comment out if frontend job owns the ingress
+kubectl apply -f output/ingress.yaml  
 
 echo "Deployment commands executed successfully."
